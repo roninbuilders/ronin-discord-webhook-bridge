71e4aa9931b94599772668ba3f90bd0547891c95
Glitch <glitch.txs@gmail.com>
2023-12-02 23:10:46 -0300

    resopnsive

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,8 +5,8 @@ type Props = {}
 function Header({}: Props) {
   return (
     <div className='width-full p-2 absolute top-16 text-center' >
-      <div className='text-5xl font-bold' >SkyMavis - Discord Webhook Bridge</div>
-      <div className='mt-8 opacity-70' >This is a simple tool to create a bridge between SkyMavis and Discord. It allows you to use SkyMavis webhooks with your Discord server.</div>
+      <div className='text-4xl sm:text-5xl font-bold' >SkyMavis - Discord Webhook Bridge</div>
+      <div className='text-base mt-8 opacity-70' >This is a simple tool to create a bridge between SkyMavis and Discord. It allows you to use SkyMavis webhooks with your Discord server.</div>
     </div>
   )
 }
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,11 @@
+import { set } from "@/api-calls";
 import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
   return (
     <div className="flex justify-around items-center w-full h-20 px-4 text-black nav">
-      <div className="flex items-center gap-2 font-bold ml-2">
+      <div onClick={()=> set.data(undefined)} className="flex items-center gap-2 font-bold ml-2">
         <Image className="mb-2" src='/puffy.png' width={40} height={51.12} alt='Webhooks' />
         <span className="hidden md:flex" >Webhook Notify</span>
       </div>
